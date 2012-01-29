4fe78bdee50c7eb21ee1a240e1c4c0ba2e35a213
Bernd Ahlers <bernd@tuneafish.de>
2012-01-29 17:57:11 +0100

    Handle disconnect event and setup a keep-alive timer.

--- lib/roboter/connectors/xmpp.rb
+++ lib/roboter/connectors/xmpp.rb
@@ -7,6 +7,7 @@ module Roboter
 
       def initialize(options = {})
         @client = Blather::Client.new
+        @keepalive = 5
 
         @jid = options[:jid]
         @nick = options[:nick]
@@ -21,11 +22,30 @@ module Roboter
         # Remove initial error handler.
         @client.clear_handlers(:error)
 
+        @client.register_handler(:disconnected, method(:handle_disconnect))
+
         @client.run
+
+        EM.add_periodic_timer(@keepalive, method(:keepalive!))
       end
 
       def stop
         @client.close
+      end
+
+      def keepalive!
+        @client.connected? ? @client.write(' ') : reconnect!
+      rescue
+        reconnect!
+      end
+
+      def reconnect!
+        @client.run
+      end
+
+      private
+      def handle_disconnect
+        true # Return true to avoid stopping the event loop.
       end
     end
   end
--- spec/roboter/connectors/xmpp_spec.rb
+++ spec/roboter/connectors/xmpp_spec.rb
@@ -19,6 +19,7 @@ describe Roboter::Connectors::XMPP do
 
   before do
     Blather::Client.stub(:new).and_return(blather)
+    EM.stub(:add_periodic_timer)
   end
 
   it "instantiates a Blather::Client object" do
@@ -39,8 +40,22 @@ describe Roboter::Connectors::XMPP do
       xmpp.start
     end
 
+    it "adds a disconnect handler that returns true" do
+      blather.should_receive(:register_handler).with do |name, method|
+        name == :disconnected and method.call == true
+      end
+      xmpp.start
+    end
+
     it "starts the client connection" do
       blather.should_receive(:run)
+      xmpp.start
+    end
+
+    it "sets up a keep-alive timer (5 sec)" do
+      EM.should_receive(:add_periodic_timer).with do |number, method|
+        number == 5 and method.name == :keepalive!
+      end
       xmpp.start
     end
   end
@@ -49,6 +64,39 @@ describe Roboter::Connectors::XMPP do
     it "closes Blather::Client connection" do
       blather.should_receive(:close)
       xmpp.stop
+    end
+  end
+
+  describe "#keepalive!" do
+    context "client connected" do
+      it "will write an empty string to the client" do
+        blather.stub(:connected?).and_return(true)
+        blather.should_receive(:write).with(' ')
+        xmpp.keepalive!
+      end
+    end
+
+    context "client disconnected" do
+      it "calls the reconnect handler" do
+        blather.stub(:connected?).and_return(false)
+        xmpp.should_receive(:reconnect!)
+        xmpp.keepalive!
+      end
+    end
+
+    context "with exception raised" do
+      it "calls the reconnect handler" do
+        blather.stub(:connected?).and_raise(RuntimeError)
+        xmpp.should_receive(:reconnect!)
+        xmpp.keepalive!
+      end
+    end
+  end
+
+  describe "#reconnect!" do
+    it "calls run on the Blather::Client" do
+      blather.should_receive(:run)
+      xmpp.reconnect!
     end
   end
 end
